69f54f24a38976fcc276b574c1ec907c262f117c
Jakub Lipinski <jakub.lipinski@gmail.com>
2020-08-14 21:13:56 +0200

    speed up x2 - numpy lexsort and clustering only those chromosome which had any changes in the previous step

--- cluster_pets/cluster_PETs.py
+++ cluster_pets/cluster_PETs.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import numba
 import functools
+import itertools
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%H:%M:%S')
 
 
@@ -12,7 +13,7 @@ def add_build_args(parser):
                         default=["~/BioData/chromatin_loops/4DNFI2BAXOSW_GM12878_CTCF_rep1_hiseq.bedpe"],
                         help=".bedpe files containing raw PETs (no header)")
     parser.add_argument("--clusters_filename", type=str,
-                        default="~/BioData/chromatin_loops/4DNFI2BAXOSW_GM12878_CTCF_rep1_hiseq.bedpe.1.9.25.py3.clusters",
+                        default="~/BioData/chromatin_loops/4DNFI2BAXOSW.bedpe.1.9.25._2clusters",
                         help=".bedpe output file with clustered PETs (no header)")
     parser.add_argument("--self_ligation", type=int, default=8000, help="Self-ligation genomic span (default: 8000)")
     parser.add_argument("--extension", type=int, default=25,
@@ -27,7 +28,7 @@ def cluster_PETs(args):
     # reading the file
     logging.info(f"Reading PETs from {args.pets_filename} ...")
     columns = ["chrom1", "start1", "end1", "chrom2", "start2", "end2", "cnt"]
-    pets = pd.concat([pd.read_csv(f, sep="\t", header=None, names=columns, low_memory=False) for f in args.pets_filename])
+    pets = pd.concat([pd.read_csv(f, sep="\t", header=None, names=columns, low_memory=False, index_col=False) for f in args.pets_filename])
     logging.info(f"Read {len(pets):,} PETs.")
 
     # pre-proccess
@@ -55,93 +56,76 @@ def cluster_PETs(args):
 
     logging.info("Done.")
 
-    chrom, start1, end1, start2, end2, cnt = \
-        pets.chrom1.to_numpy(dtype=str, copy=True), pets.start1.to_numpy(copy=True), pets.end1.to_numpy(copy=True),\
-        pets.start2.to_numpy(copy=True), pets.end2.to_numpy(copy=True), pets.cnt.to_numpy(copy=True)
+    chroms = pets.groupby(["chrom1"]).size().to_dict()
 
-    chrom_idxs = None
+    start1s = numba.typed.List([pets[pets.chrom1 == chrom].start1.to_numpy() for chrom in chroms.keys()])
+    end1s = numba.typed.List([pets[pets.chrom1 == chrom].end1.to_numpy() for chrom in chroms.keys()])
+    start2s = numba.typed.List([pets[pets.chrom1 == chrom].start2.to_numpy() for chrom in chroms.keys()])
+    end2s = numba.typed.List([pets[pets.chrom1 == chrom].end2.to_numpy() for chrom in chroms.keys()])
+    cnts = numba.typed.List([pets[pets.chrom1 == chrom].cnt.to_numpy() for chrom in chroms.keys()])
+
     step = 0
-    order = np.arange(len(chrom))
-    while True:
-        # sorting
+    changes = np.ones(shape=(len(chroms),))
+    while np.sum(changes) > 0:
+        # sorting# if changes[i] > 0 else None\
         logging.info(f"Sorting (step: #{step+1}, PETs: {len(pets):,}) ...")
-        @numba.jit(nopython=True)
-        def cmp(i, j):
-            if chrom[i] != chrom[j]:
-                return -1 if chrom[i] < chrom[j] else 1
-            if start1[i] != start1[j]:
-                return start1[i] - start1[j]
-            if end1[i] != end1[j]:
-                return end1[i] - end1[j]
-            if start2[i] != start2[j]:
-                return start2[i] - start2[j]
-            if end2[i] != end2[j]:
-                return end2[i] - end2[j]
-            return 0
-
-        order = list(order)
-        order.sort(key=functools.cmp_to_key(cmp))
-        order = np.array(order)
+        orders = numba.typed.List([np.lexsort((end2s[i], start2s[i], end1s[i], start1s[i]))
+                                   for i in range(len(chroms))])
         logging.info("Done.")
-
-        @numba.jit(nopython=True)
-        def calc_chrom_idxs(chrom, order):
-            chrom_idxs = numba.typed.List()
-            old_chrom = chrom[0]
-            for _i in range(len(chrom)):
-                i = order[_i]
-                if _i == 0 or chrom[i] != old_chrom:
-                    chrom_idxs.append(_i)
-                    old_chrom = chrom[i]
-            chrom_idxs.append(len(chrom))
-            return chrom_idxs
-
-        if not chrom_idxs:
-            chrom_idxs = calc_chrom_idxs(chrom, order)
 
         # clustering
         logging.info(f"Clustering (step: #{step+1}, PETs: {len(pets):,}) ...")
 
         @numba.jit(nopython=True, parallel=True)
-        def cluster(chrom_idxs, order, start1, end1, start2, end2, cnt):
-            changes = np.zeros(shape=(len(chrom_idxs)-1,), dtype=np.uint64)
+        def cluster(chroms, old_changes, orders, start1s, end1s, start2s, end2s, cnts):
+            changes = np.zeros(shape=(len(chroms)-1,), dtype=np.uint64)
 
-            for idx in numba.prange(len(chrom_idxs)-1):
-                for _i in range(chrom_idxs[idx], chrom_idxs[idx+1]):
-                    i = order[_i]
-                    if cnt[i] == 0:
-                        continue
-                    _j = _i + 1
-                    while _j < chrom_idxs[idx+1]:
-                        j = order[_j]
-                        if start1[j] > end1[i]:
-                            break
-                        if cnt[j] == 0:
-                            _j += 1
+            for idx in numba.prange(len(chroms)-1):
+                if old_changes[idx] > 0:
+                    order, start1, end1, start2, end2, cnt =\
+                        orders[idx], start1s[idx], end1s[idx], start2s[idx], end2s[idx], cnts[idx]
+                    for _i in range(chroms[idx]):
+                        i = order[_i]
+                        if cnt[i] == 0:
                             continue
-                        if ((start1[i] <= start1[j] and start1[j] <= end1[i]) or (start1[i] <= end1[j] and end1[j] <= end1[i])) and\
-                           ((start2[i] <= start2[j] and start2[j] <= end2[i]) or (start2[i] <= end2[j] and end2[j] <= end2[i])):
-                            start1[i] = min(start1[i], start1[j])
-                            end1[i] = max(end1[i], end1[j])
-                            start2[i] = min(start2[i], start2[j])
-                            end2[i] = max(end2[i], end2[j])
-                            cnt[i] += cnt[j]
-                            cnt[j] = 0
-                            changes[idx] += 1
-                        _j += 1
+                        _j = _i + 1
+                        while _j < chroms[idx]:
+                            j = order[_j]
+                            if start1[j] > end1[i]:
+                                break
+                            if cnt[j] == 0:
+                                _j += 1
+                                continue
+                            if ((start1[i] <= start1[j] and start1[j] <= end1[i]) or (start1[i] <= end1[j] and end1[j] <= end1[i])) and\
+                               ((start2[i] <= start2[j] and start2[j] <= end2[i]) or (start2[i] <= end2[j] and end2[j] <= end2[i])):
+                                start1[i] = min(start1[i], start1[j])
+                                end1[i] = max(end1[i], end1[j])
+                                start2[i] = min(start2[i], start2[j])
+                                end2[i] = max(end2[i], end2[j])
+                                cnt[i] += cnt[j]
+                                cnt[j] = 0
+                                changes[idx] += 1
+                            _j += 1
             return changes
-        changes = cluster(chrom_idxs, order, start1, end1, start2, end2, cnt)
-        sum_changes = int(sum(changes))
-        logging.info(f"Done. Changes: {sum_changes:,}")
-        if sum_changes == 0:
-            break
+        changes = cluster(numba.typed.List(chroms.values()), changes, orders, start1s, end1s, start2s, end2s, cnts)
+        logging.info(f"Done. Changes: {int(sum(changes)):,}")
         step += 1
 
     # save to file
     logging.info(f"Saving to {args.clusters_filename} (cluster cufoff: {args.cluster_cutoff})... ")
-    pets = pd.DataFrame(data={"chrom1": chrom[order], "start1": start1[order], "end1": end1[order],
-                              "chrom2": chrom[order], "start2": start2[order], "end2": end2[order],
-                              "cnt": cnt[order]})
+    pets = pd.DataFrame(data={"chrom1": itertools.chain.from_iterable([itertools.repeat(chrom, size) for chrom, size in chroms.items()]),
+                              "start1": itertools.chain.from_iterable([start1s[i][orders[i]] for i in range(len(chroms))]),
+                              "end1": itertools.chain.from_iterable([end1s[i][orders[i]] for i in range(len(chroms))]),
+                              "chrom2": itertools.chain.from_iterable([itertools.repeat(chrom, size) for chrom, size in chroms.items()]),
+                              "start2": itertools.chain.from_iterable([start2s[i][orders[i]] for i in range(len(chroms))]),
+                              "end2": itertools.chain.from_iterable([end2s[i][orders[i]] for i in range(len(chroms))]),
+                              "cnt": itertools.chain.from_iterable([cnts[i][orders[i]] for i in range(len(chroms))])
+                              })
+    print(pets.head(20))
+    print(pets.tail(20))
+#                            "start1": start1[order], "end1": end1[order],
+#                              "chrom2": chrom[order], "start2": start2[order], "end2": end2[order],
+#                              "cnt": cnt[order]})
     pets = pets[pets.cnt >= args.cluster_cutoff]
     pets.to_csv(args.clusters_filename, sep="\t", index=False, header=False)
     logging.info(f"Done. Saved {len(pets)} clusters.")
